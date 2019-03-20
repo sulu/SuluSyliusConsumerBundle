d6015c48dd173b1c0af45f59c499913bd1ebeeb5
Patrick Hopfner <patrick.hopfner@gmail.com>
2019-03-20 14:27:03 +0100

    Add "AddressOrderMessage" (#69)

---
+++ Tests/Functional/Model/Order/Handler/AddressOrderMessageTest.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\SyliusConsumerBundle\Tests\Functional\Model\Order\Handler;
+
+use GuzzleHttp\Psr7\Response;
+use Sulu\Bundle\SyliusConsumerBundle\Model\Address\Address;
+use Sulu\Bundle\SyliusConsumerBundle\Model\Order\Message\AddressOrderMessage;
+use Sulu\Bundle\SyliusConsumerBundle\Tests\Service\GatewayClient;
+use Sulu\Bundle\TestBundle\Testing\SuluTestCase;
+use Symfony\Component\Messenger\MessageBus;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class AddressOrderMessageTest extends SuluTestCase
+{
+    public function setUp()
+    {
+        $this->purgeDatabase();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function getKernelConfiguration()
+    {
+        return ['sulu_context' => 'website'];
+    }
+
+    public function testMin(): void
+    {
+        $this->getGatewayClient()->setHandleRequestCallable(
+            function ($method, $uri, array $options = []) {
+                $this->assertEquals('PUT', $method);
+                $this->assertEquals('/api/v1/checkouts/addressing/21', $uri);
+                $this->assertEquals(
+                    [
+                        'differentBillingAddress' => false,
+                        'shippingAddress' => [
+                            'firstName' => 'Elon',
+                            'lastName' => 'Musk',
+                            'street' => '10941 Savona Rd',
+                            'postcode' => 'CA 900077',
+                            'city' => 'Los Angeles',
+                            'countryCode' => 'US',
+                        ],
+                    ],
+                    $options['json']
+                );
+
+                return new Response(204, []);
+            }
+        );
+
+        $orderId = 21;
+        $shippingAddress = new Address('Elon', 'Musk', '10941 Savona Rd', 'CA 900077', 'Los Angeles', 'US');
+
+        $message = new AddressOrderMessage($orderId, $shippingAddress);
+
+        // send message
+        $this->getMessageBus()->dispatch($message);
+    }
+
+    public function testMax(): void
+    {
+        $this->getGatewayClient()->setHandleRequestCallable(
+            function ($method, $uri, array $options = []) {
+                $this->assertEquals('PUT', $method);
+                $this->assertEquals('/api/v1/checkouts/addressing/21', $uri);
+                $this->assertEquals(
+                    [
+                        'differentBillingAddress' => true,
+                        'shippingAddress' => [
+                            'firstName' => 'Elon',
+                            'lastName' => 'Musk',
+                            'street' => '10941 Savona Rd',
+                            'postcode' => 'CA 900077',
+                            'city' => 'Los Angeles',
+                            'countryCode' => 'US',
+                        ],
+                        'billingAddress' => [
+                            'firstName' => 'Elon',
+                            'lastName' => 'Musk',
+                            'street' => 'Tesla Street 123',
+                            'postcode' => 'CA 900077',
+                            'city' => 'Los Angeles',
+                            'countryCode' => 'US',
+                        ],
+                    ],
+                    $options['json']
+                );
+
+                return new Response(204, []);
+            }
+        );
+
+        $orderId = 21;
+        $shippingAddress = new Address('Elon', 'Musk', '10941 Savona Rd', 'CA 900077', 'Los Angeles', 'US');
+        $billingAddress = new Address('Elon', 'Musk', 'Tesla Street 123', 'CA 900077', 'Los Angeles', 'US');
+
+        $message = new AddressOrderMessage($orderId, $shippingAddress, $billingAddress);
+
+        // send message
+        $this->getMessageBus()->dispatch($message);
+    }
+
+    private function getGatewayClient(): GatewayClient
+    {
+        /** @var GatewayClient $gatewayClient */
+        $gatewayClient = $this->getContainer()->get('sulu_sylius_consumer.gateway_client');
+
+        return $gatewayClient;
+    }
+
+    private function getMessageBus(): MessageBusInterface
+    {
+        /** @var MessageBus $messageBus */
+        $messageBus = $this->getContainer()->get('message_bus');
+
+        return $messageBus;
+    }
+}
