912ab2a7cc13ee1988e3bf9d87afebf9d9738a13
sabinebaer <sabine.baer@massiveart.com>
2019-06-07 01:07:53 +0200

    Fixed routing

--- DependencyInjection/SuluSyliusConsumerExtension.php
+++ DependencyInjection/SuluSyliusConsumerExtension.php
@@ -195,8 +195,8 @@ class SuluSyliusConsumerExtension extends Extension implements PrependExtensionI
                 'resources' => [
                     ProductInterface::RESOURCE_KEY => [
                         'routes' => [
-                            'list' => 'sulu_sylius_product.products_list',
-                            'detail' => 'sulu_sylius_product.product_edit_form',
+                            'list' => 'get_products',
+                            'detail' => 'get_product',
                         ],
                     ],
                 ],
