156e0aced32f255265ac7bb1b99242b94cac79ef
Johannes Wachter <johannes@sulu.io>
2019-08-01 10:15:50 +0200

    add remove taxon handler

--- Repository/Category/CategoryRepository.php
+++ Repository/Category/CategoryRepository.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Sulu\Bundle\SyliusConsumerBundle\Repository\Category;
 
+use Doctrine\ORM\NonUniqueResultException;
 use Sulu\Bundle\CategoryBundle\Entity\CategoryRepository as BaseCategoryRepository;
 use Sulu\Bundle\SyliusConsumerBundle\Model\Category\CategoryInterface;
 use Sulu\Bundle\SyliusConsumerBundle\Model\Category\CategoryRepositoryInterface;
@@ -25,6 +26,22 @@ class CategoryRepository extends BaseCategoryRepository implements CategoryRepos
         $category = $this->findOneBy(['syliusId' => $syliusId]);
 
         return $category;
+    }
+
+    public function findIdBySyliusId(int $syliusId): ?int
+    {
+        try {
+            $result = $this->createQueryBuilder('c')
+                ->select('c.id')
+                ->where('c.syliusId = :syliusId')
+                ->setParameter('syliusId', $syliusId)
+                ->getQuery()
+                ->getSingleScalarResult();
+        } catch (NonUniqueResultException $exception) {
+            return null;
+        }
+
+        return (int) $result;
     }
 
     public function persist(CategoryInterface $category): void
