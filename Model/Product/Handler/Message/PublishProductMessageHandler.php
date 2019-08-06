27173c0e0f9c23ee13ba1eb99cfe8151723037a0
rs2487 <raphael.stocker@massiveart.com>
2019-08-06 08:38:03 +0200

    Updated urls in tests for route generation

--- Model/Product/Handler/Message/PublishProductMessageHandler.php
+++ Model/Product/Handler/Message/PublishProductMessageHandler.php
@@ -110,9 +110,6 @@ class PublishProductMessageHandler
             new PublishContentMessage(ProductInterface::CONTENT_RESOURCE_KEY, $message->getId(), $message->getLocale(), false)
         );
 
-        var_dump($this->routeMappings); exit;
-
-
         $mappings = $this->routeMappings[RoutableResource::class];
         $routePath = $this->routeGenerator->generate($product, $mappings['options']);
 
