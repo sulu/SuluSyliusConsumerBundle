a7f10e1f812af39c8eaa30ad43c66db8b2cfc972
rs2487 <raphael.stocker@massiveart.com>
2020-09-23 14:55:26 +0200

    Fixed phpstan errors

--- Tests/Functional/Traits/ProductInformationTrait.php
+++ Tests/Functional/Traits/ProductInformationTrait.php
@@ -29,6 +29,7 @@ trait ProductInformationTrait
             ]
         );
 
+        /** @var ProductInterface $product */
         $product = $this->getEntityManager()->find(Product::class, $productId);
         if (!$product) {
             throw new \RuntimeException('Product not fount');
